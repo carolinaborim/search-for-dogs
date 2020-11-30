c76c151db7d7bfa70a4f465d213869b40aed7dd4
Carolina Borim <carolina.borim@gmail.com>
2020-11-30 20:11:42 +0100

    Renames test to match function

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import Dog from './components/Dog.js';
 
-test('renders learn react link', () => {
+test('renders button for render image', () => {
   render(<Dog />);
   const linkElement = screen.getByText("Show Me a Doggo");
   expect(linkElement).toBeInTheDocument();
