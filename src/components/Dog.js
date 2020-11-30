eff9cf9128c05fcfbae7f4c4b35a0a7022789a30
Carolina Borim <carolina.borim@gmail.com>
2020-11-30 18:38:04 +0100

    Adds axios and performs call to the API

--- src/components/Dog.js
+++ src/components/Dog.js
@@ -1,14 +1,16 @@
 import React from "react";
+import axios from "axios";
 
 class Dog extends React.Component {
-  onButtonClick = event => {
-    console.log('Click Button');
+  async onButtonClick() {
+    const response = await axios.get("https://dog.ceo/api/breeds/image/random");
+    console.log(response);
   };
 
   render() {
     return (
       <div className="ui segment">
-        <button  class="ui secondary button" onClick={this.onButtonClick.bind(this)}>Show Me a Doggo </button>
+        <button  className="ui secondary button" onClick={this.onButtonClick.bind(this)}>Show Me a Doggo </button>
       </div>
     );
   }
