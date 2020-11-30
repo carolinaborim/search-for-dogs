6de84719bd86e2b76aa8fb715c2e6123c6a49e60
Carolina Borim <carolina.borim@gmail.com>
2020-11-30 19:25:15 +0100

    Renders image when clicking button 	- also adds styles

--- src/components/Dog.js
+++ src/components/Dog.js
@@ -1,16 +1,22 @@
 import React from "react";
 import axios from "axios";
+import '../App.css';
 
 class Dog extends React.Component {
+  state = { images: {} };
+
   async onButtonClick() {
     const response = await axios.get("https://dog.ceo/api/breeds/image/random");
-    console.log(response);
+    this.setState({ image: response.data.message });
   };
 
   render() {
     return (
-      <div className="ui segment">
-        <button  className="ui secondary button" onClick={this.onButtonClick.bind(this)}>Show Me a Doggo </button>
+      <div className="container">
+        <button className="ui violet basic button btn" onClick={this.onButtonClick.bind(this)}>Show Me a Doggo </button>
+        <div className="image">
+            <img className="ui medium circular image" src={this.state.image} />
+        </div>
       </div>
     );
   }
