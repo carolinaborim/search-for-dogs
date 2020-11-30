3154924df60e25e535b9e56e189c09e0ed7a612d
Carolina Borim <carolina.borim@gmail.com>
2020-11-30 20:09:04 +0100

    Changes test.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Dog from './components/Dog.js';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(<Dog />);
+  const linkElement = screen.getByText("Show Me a Doggo");
   expect(linkElement).toBeInTheDocument();
 });
